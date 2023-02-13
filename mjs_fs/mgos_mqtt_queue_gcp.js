288684a0b67ab1d0c852a8524cefe814b3aa5674
RafalConnectIQ <120720209+RafalConnectIQ@users.noreply.github.com>
2023-02-13 18:04:39 +0000

    Update mgos_mqtt_queue_gcp.js

--- mjs_fs/mgos_mqtt_queue_gcp.js
+++ mjs_fs/mgos_mqtt_queue_gcp.js
@@ -1,6 +1,7 @@
 let MQTT_queue = {
-  _pub: ffi('bool mgos_mqtt_queue_gcp_send_event_subf(char *, void *)'),
+  _pub: ffi('bool mgos_mqtt_queue_send_event_pub_json(char *, char * )'),
 
+  //_pub: ffi('bool mgos_mqtt_queue_gcp_send_event_subf(char *, void *)'),
   // ## **`MQTT_queue.pub(topic, message)`**
   // Publish message to a topic. 
   // Return value: 0 on failure (e.g. no connection to server), 1 on success.
